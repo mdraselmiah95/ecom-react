7e7d0b6a8eb1d6388c0711e5b70e3bbb5262c1bf
rasel <russelroy15@gmail.com>
2021-09-25 17:35:19 +0600

    WIP on Ecom React

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -6,25 +6,28 @@ const Cart = (props) => {
 
   //Use Reduce in Cart as Cart is array od object
   const totalReducer = (previous, current) => previous + current.price;
-  const total = cart.reduce(totalReducer, 0).toFixed(2);
-  const tax = 0.1 * total;
-  const totalPrice = total + tax;
+  const total = cart.reduce(totalReducer, 0);
+  const shipping = total > 0 ? 15 : 0;
+  const tax = (total + shipping) * 0.1;
+  const grandTotal = total + shipping + tax;
 
   return (
     <div>
       <h2>
         <i className="fas fa-shopping-cart">{cart.length}</i>
       </h2>
-      <h2>{total}</h2>
-      <h3>Tax:{tax}</h3>
-      <h2>Total price: {totalPrice}</h2>
-      {/* <ul>
-        {cart.map((product) => (
-          <li>{product.name}</li>
-        ))}
-      </ul> */}
+      <h2>{total.toFixed(2)}</h2>
+      <h3>Tax:{tax.toFixed(2)}</h3>
+      <h2>Total price: {grandTotal.toFixed(2)}</h2>
     </div>
   );
 };
 
+/* 
+    <ul>
+        {cart.map((product) => (
+          <li>{product.name}</li>
+        ))}
+      </ul>
+*/
 export default Cart;
