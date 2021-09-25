1121fdf22cf6ca8ebfd332c5e552f6deb5813ca1
rasel <russelroy15@gmail.com>
2021-09-25 15:28:33 +0600

    WIP on Ecom React

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,10 +1,13 @@
 import React from "react";
+import Product from "../Product/Product";
 import "./Shop.css";
 const Shop = () => {
   return (
     <div>
       <div className="row">
-        <div className="col-md-9">Product load</div>
+        <div className="col-md-9">
+          <Product />
+        </div>
         <div className="col-md-3">Cart calculation</div>
       </div>
     </div>
