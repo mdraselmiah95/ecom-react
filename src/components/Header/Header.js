358f2bb717ea770e0643b4e534dc4901cc1d4ec4
rasel <russelroy15@gmail.com>
2021-09-25 15:15:07 +0600

    WIP on Ecom React

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -21,11 +21,6 @@ const Header = () => {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <a className="nav-link" aria-current="page" href="/new">
-                Home
-              </a>
-            </li>
-            <li className="nav-item">
               <a className="nav-link" href="/new">
                 Shops
               </a>
