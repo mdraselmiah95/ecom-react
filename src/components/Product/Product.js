7f6b045eca0036671e4949410c237345dcf7f32a
rasel <russelroy15@gmail.com>
2021-09-25 15:38:45 +0600

    WIP on Ecom React

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -5,10 +5,10 @@ const Product = () => {
     <div className="col-md-6">
       <div className="card mb-3" style={{ maxWidth: "540px" }}>
         <div className="row g-0">
-          <div className="col-md-4">
+          <div className="col-md-5">
             <img src="..." className="img-fluid rounded-start" alt="" />
           </div>
-          <div className="col-md-8">
+          <div className="col-md-7">
             <div className="card-body">
               <h5 className="card-title">Card title</h5>
               <p className="card-text">
