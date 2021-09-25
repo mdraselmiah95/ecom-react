99b80dee5ee44f51cde18e4da329aea300b88c0e
rasel <russelroy15@gmail.com>
2021-09-25 15:37:27 +0600

    WIP on Ecom React

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Product from "../Product/Product";
 import "./Shop.css";
 const Shop = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      "https://raw.githubusercontent.com/ProgrammingHero1/ema-john-simple-resources/master/fakeData/products.JSON"
+    )
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
+
   return (
     <div>
       <div className="row">
         <div className="col-md-9">
-          <Product />
+          <div className="row">
+            <Product />
+            <Product />
+            <Product />
+          </div>
         </div>
         <div className="col-md-3">Cart calculation</div>
       </div>
